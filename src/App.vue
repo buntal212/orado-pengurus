17ee93ba6e72d657073ca84dcb032d4544181f59
kentangkotak <ariezlnd69@gmail.com>
2026-08-27 00:13:41 +0700

    logo

--- src/App.vue
+++ src/App.vue
@@ -4,5 +4,14 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue'
 import PwaControl from '@/components/PwaControl.vue'
+
+onMounted(() => {
+  const splash = document.getElementById('orado-splash')
+  if (!splash) return
+
+  splash.classList.add('is-hidden')
+  window.setTimeout(() => splash.remove(), 220)
+})
 </script>
