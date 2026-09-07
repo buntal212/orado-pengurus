ee9e9ffa6989877f2c18e9276b56f9e8dd7675d6
kentangkotak <ariezlnd69@gmail.com>
2026-09-07 18:08:37 +0700

    sasa

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -6,6 +6,8 @@ const api = axios.create({
   headers: { Accept: 'application/json' },
 })
 
+let redirectingToLogin = false
+
 api.interceptors.request.use((config) => {
   const token =
     localStorage.getItem('orado_pengurus_token') ||
@@ -14,6 +16,29 @@ api.interceptors.request.use((config) => {
   if (token) config.headers.Authorization = `Bearer ${token}`
   return config
 })
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401 && !redirectingToLogin) {
+      const hasSession = Boolean(
+        localStorage.getItem('orado_pengurus_token') ||
+          sessionStorage.getItem('orado_pengurus_token'),
+      )
+
+      if (hasSession) {
+        redirectingToLogin = true
+        localStorage.removeItem('orado_pengurus_token')
+        sessionStorage.removeItem('orado_pengurus_token')
+        localStorage.removeItem('orado_pengurus_user')
+        sessionStorage.removeItem('orado_pengurus_user')
+        window.location.replace('/login')
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
 
 export default boot(({ app }) => {
   app.config.globalProperties.$axios = axios
