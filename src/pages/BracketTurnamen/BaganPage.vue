0368a16b502cc067d898e498734982cb18e1afa1
kentangkotak <ariezlnd69@gmail.com>
2026-09-21 16:48:47 +0700

    asd

--- src/pages/BracketTurnamen/BaganPage.vue
+++ src/pages/BracketTurnamen/BaganPage.vue
@@ -181,7 +181,7 @@
         aria-label="Pesan semangat pertandingan"
       >
         <span>
-          Selamat Bertanding, Junjung Sportifitas ...... #DominoNaikKelas #OradoMenencerdaskanIndonesia #OradoProbolinggoOdikTerus
+          Selamat Bertanding, Junjung Sportifitas ...... #DominoNaikKelas #OradoMemintarkanIndonesia #OradoProbolinggoOdikTerus
         </span>
       </div>
 
